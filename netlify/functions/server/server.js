de98e1f3a060fcfc82e43f3983683e0a93fb7f95
Logan McAnsh <logan@mcan.sh>
2021-09-11 17:35:30 -0500

    chore: remove log
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- netlify/functions/server/server.js
+++ netlify/functions/server/server.js
@@ -4,17 +4,17 @@ const path = require("path");
 const BUILD_DIR = path.join(process.cwd(), "netlify");
 
 exports.handler =
-  process.env.NODE_ENV === "development"
-    ? (event, context) => {
-        console.log("event", event);
+  process.env.NODE_ENV === "production"
+    ? createRequestHandler({ build: require("./build") })
+    : (event, context) => {
         for (const key of Object.keys(require.cache)) {
-          console.log(key);
           if (key.startsWith(BUILD_DIR)) {
             delete require.cache[key];
           }
         }
-        return createRequestHandler({
-          build: require("./build"),
-        })(event, context);
-      }
-    : createRequestHandler({ build: require("./build") });
+
+        return createRequestHandler({ build: require("./build") })(
+          event,
+          context
+        );
+      };
